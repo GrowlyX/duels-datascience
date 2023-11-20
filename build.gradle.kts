34f9c41aa6a2a67ba1bb8da9dadaa3761b99b726
GrowlyX <growlyxdev@gmail.com>
2023-11-19 21:39:25 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.serialization") version "1.9.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    application
+}
+
+group = "us.duels.datascience"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.litote.kmongo:kmongo-serialization:4.10.0")
+
+    implementation("com.xenomachina:kotlin-argparser:2.0.7")
+}
+
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("")
+    archiveFileName.set(
+        "datascience.jar"
+    )
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
